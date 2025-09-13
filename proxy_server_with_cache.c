aec6343cb84a2de8291bcfc59e76b76c494548d5
11Strange11 <anuragpipil3@gmail.com>
2025-09-13 11:40:25 +0530

    Implemented checkHTTPversion function

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -183,6 +183,23 @@ int handle_request(int clientSocket, ParsedRequest *request, char *tempReq){
 	return 0;
 }
 
+int checkHTTPversion(char* msg){
+    int version = -1;
+    if(strncmp(msg, "HTTP/1.1",8) == 0){
+        version = 1;
+    }
+    else if(strncmp(msg, "HTTP/1.0", 8) == 0){
+        version = 1;
+    }
+    else if(strncmp(msg, "HTTP/1.0", 8) == 0){
+        version = 1;
+    }
+    else{
+        version = -1;
+    }
+    return version;
+}
+
 
 void* thread_fn(void* socketNew){
     //checking if the semaphore is available (meaning the cache is not full)
